32977d79a741998df2c2eb1f2bdac28afb10e655
iryna-romashchenko <irromas@ukr.net>
2022-08-16 18:54:11 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+ const list = document.querySelector('#ingredients');
+ console.log(list);
+for (let ingredient of ingredients) {
+  
+
+const listItem = document.createElement('li');
+listItem.textContent = ingredient;
+listItem.classList.add('item');
+list.append(listItem);}
+
