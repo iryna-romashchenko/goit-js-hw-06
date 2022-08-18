1dcdde57b59378289dd1e2e94591b12672e84638
iryna-romashchenko <irromas@ukr.net>
2022-08-18 18:42:42 +0300

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const inputEl = document.querySelector('#name-input');
+const spanEl = document.querySelector('#name-output')
+
+const inputName = () => {
+    spanEl.textContent = inputEl.value
+    if (inputEl.value.trim() === "".trim()) {
+        spanEl.textContent = 'Anonymous'
+    }
+}
+
+inputEl.addEventListener('input', inputName)
