00456fe06002f9f17ca2e53f4fcc1444e9a3507b
iryna-romashchenko <irromas@ukr.net>
2022-08-19 11:01:58 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+const randomColor = document.querySelector('.widget');
+const span = document.querySelector('.color');
+randomColor.addEventListener('click', onButtonClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onButtonClick(evt) {
+  span.textContent = getRandomHexColor();
+  if (!evt.target.classList.contains('change-color')) {
+    return;
+  }
+  return (document.body.style.background = getRandomHexColor());
 }
