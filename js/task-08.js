09a1755231a2125c955fe7e13235c4e126e2d22b
iryna-romashchenko <irromas@ukr.net>
2022-08-19 11:01:55 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formElement = event.currentTarget.elements;
+  const email = formElement.email.value;
+  const password = formElement.password.value;
+  const formData = { email, password };
+  if (email === '' || password === '') {
+    alert('all fields must be filled');
+  } else {
+    console.log(formData);
+  }
+
+  form.reset();
+}
