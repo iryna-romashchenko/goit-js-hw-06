94c40c737563084543f460bf71df8e68a87d2f7c
iryna-romashchenko <irromas@ukr.net>
2022-08-20 11:54:13 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,19 +1,16 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
 
-const randomColor = document.querySelector('.widget');
-const span = document.querySelector('.color');
-randomColor.addEventListener('click', onButtonClick);
+const btn = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+const body = document.querySelector("body");
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-function onButtonClick(evt) {
-  span.textContent = getRandomHexColor();
-  if (!evt.target.classList.contains('change-color')) {
-    return;
-  }
-  return (document.body.style.background = getRandomHexColor());
-}
+const onButtonClick = (event) => {
+  event.preventDefault();
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = body.style.backgroundColor;
+};
+
+btn.addEventListener("click", onButtonClick);
