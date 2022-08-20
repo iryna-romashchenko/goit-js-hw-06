a6b5b6375c4e7aa41a4247e2184cbb29a077e525
iryna-romashchenko <irromas@ukr.net>
2022-08-20 10:06:20 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,10 @@
 const inputEl = document.querySelector('#font-size-control');
 let spanEl = document.querySelector('#text');
-spanEl.style.fontSize = '0px';
 
-const editValue = () => {
+const editValue = (event) => {
+    event.preventDefault();
   spanEl.style.fontSize = `${inputEl.value}px`;
 };
 
 inputEl.addEventListener('input', editValue);
+
